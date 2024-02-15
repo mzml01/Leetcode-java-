b5fb8446d12beaa0a0c10e45524659b34f079690
Mudda <mohdmzml1001@gmail.com>
2024-02-15 17:48:45 +0530

    Commit validParenthersis,valid anagram,

---
+++ src/VaidParenthesis/ValidParenthesis2.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParenthesis2 {
+    public boolean validParenthesis(String s){
+        Stack<Character> stack=new Stack<>();
+        Map<Character,Character> bracket=new HashMap<>();
+        bracket.put(')','(');
+        bracket.put(']','[');
+        bracket.put('}','{');
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(bracket.containsKey(c)){
+                if(!bracket.isEmpty() && bracket.get(c).equals(stack.peek())){
+                    stack.pop();
+                }
+
+            }else stack.push(s.charAt(i));
+        }
+    return stack.isEmpty();
+    }
+    public static void main(String[] args){
+        String s="[]{}()";
+        ValidParenthesis obj=new ValidParenthesis();
+        System.out.println(obj.isValid(s));
+    }
+}
