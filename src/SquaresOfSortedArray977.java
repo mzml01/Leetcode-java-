a7a9baedae4ac0cdaea1a45faa118b3ddd2fbd6c
Mudda <mohdmzml1001@gmail.com>
2024-03-02 09:31:29 +0530

    SquaresOfSortedArray 2

--- src/SquaresOfSortedArray977.java
+++ src/SquaresOfSortedArray977.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class SquaresOfSortedArray977 {
+import java.util.Arrays;
+
+public class SquaresOfSortedArray977 {
+    public int[] Solution(int[] arr){
+        for(int i=0;i< arr.length;i++){
+            arr[i]=(int)Math.pow(arr[i],2);
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+    public static void main(String[] args){
+        int[] arr={-4,-1,0,3,10};
+        SquaresOfSortedArray977 obj=new SquaresOfSortedArray977();
+        for(int i : obj.Solution(arr)){
+            System.out.println(i);
+        }
+    }
 }
